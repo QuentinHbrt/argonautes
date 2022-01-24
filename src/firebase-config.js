77e9c4a9b13530b4c517d60861270df01aa0996c
QuentinHbrt <quentin.hebert@hotmail.fr>
2022-01-24 15:22:32 +0100

    add: firebase

---
+++ src/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBodHaghrCtmmh7XDKXoiADVGPH3m2B5h0",
+  authDomain: "argonaute-49349.firebaseapp.com",
+  projectId: "argonaute-49349",
+  storageBucket: "argonaute-49349.appspot.com",
+  messagingSenderId: "229727054252",
+  appId: "1:229727054252:web:959d6736ed2d00c6387209",
+  measurementId: "G-GX2NTTHQN4",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
